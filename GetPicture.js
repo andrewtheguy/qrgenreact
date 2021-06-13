ff9f189f54b0938c2c2533790322182b6bd5ce62
Andrew Chen <andrew@jaanuu.com>
2021-06-12 19:57:08 -0700

    testing

--- GetPicture.js
+++ GetPicture.js
@@ -1,103 +1,44 @@
-'use strict';
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-import React, {useEffect, useState} from 'react';
-import CameraRoll from "@react-native-community/cameraroll";
-
-import {
-    StyleSheet,
-    Text,
-    Button,
-    TouchableOpacity,
-    Linking,
-    SafeAreaView,
-    ScrollView, TextInput, Image, TouchableHighlight, ListView, FlatList, Alert, View,
-} from 'react-native';
-import QRCode from 'react-native-qrcode-svg';
 
 export default function GetPicture() {
+    const [image, setImage] = useState(null);
 
-    const [photos, setPhotos] = useState(null);
-    const [selectedPhoto,setSelectedPhoto] = useState(null);
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        })();
+    }, []);
 
-    const handleButtonPress = () => {
-        console.log("handleButtonPress")
-        CameraRoll.getPhotos({
-            first: 5000,
-            assetType: 'Photos',
-        })
-            .then(r => {
-                console.log(r);
-                setPhotos(r.edges);
-            })
-            .catch((err) => {
-                console.error(err);
-                Alert.alert("Error Loading Images")
-                //Error Loading Images
-            });
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: false,
+            //aspect: [4, 3],
+            //quality: 1,
+            base64: false,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+            console.log(result);
+
+        }
     };
 
-
-    const renderRow = (rowData) => {
-        console.log(rowData.item.node);
-        //return null;
-
-        const obj = rowData.item.node;
-        return (
-            <TouchableHighlight
-                onPress={() => setSelectedPhoto(obj.image.uri) }>
-                <View>
-                <Image
-                    source={{ uri: obj.image.uri }}
-                    style={styles.image} /><Text>{new Date(obj.timestamp*1000).toLocaleString("en-US")}</Text>
-                </View>
-            </TouchableHighlight>
-        )
-    }
-
     return (
-        <SafeAreaView>
-                <Button
-                    onPress={handleButtonPress}
-                    title="Get Pictures"
-                    accessibilityLabel="Get Pictures"
-                />
-            <FlatList
-            data={photos}
-            renderItem={(rowData) => renderRow(rowData)}
-             />
-        </SafeAreaView>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button title="Pick an image from camera roll" onPress={pickImage} />
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        </View>
     );
-
 }
-
-const styles = StyleSheet.create({
-    centerText: {
-        flex: 1,
-        fontSize: 18,
-        padding: 32,
-        color: '#777',
-    },
-    textBold: {
-        fontWeight: '500',
-        color: '#000',
-    },
-    buttonText: {
-        fontSize: 21,
-        color: 'rgb(0,122,255)',
-    },
-    buttonTouchable: {
-        padding: 16,
-    },
-    resultText: {
-        padding: 16,
-    },
-    image: {
-        width: 120,
-        height: 130,
-        marginLeft: 15,
-        marginTop: 15,
-        borderRadius: 6,
-        borderWidth: 2,
-        borderColor: '#efefef',
-    },
-});
