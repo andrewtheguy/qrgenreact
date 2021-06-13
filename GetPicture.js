6ab83aababd108858cce189d19e4fad6f60647ac
Andrew Chen <andrew@jaanuu.com>
2021-06-13 09:54:39 -0700

    testing

--- GetPicture.js
+++ GetPicture.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import {Button, Image, View, Platform, Text} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import {BarCodeScanner} from 'expo-barcode-scanner';
 
 
 export default function GetPicture() {
     const [image, setImage] = useState(null);
+    const [data, setData] = useState(null);
 
     useEffect(() => {
         (async () => {
@@ -30,8 +32,17 @@ export default function GetPicture() {
 
         if (!result.cancelled) {
             setImage(result.uri);
+            setData(null);
             console.log(result);
-
+            BarCodeScanner.scanFromURLAsync(result.uri).then(result=> {
+                console.log(result);
+                if(result[0]) {
+                    setData(result[0].data);
+                }
+            }).catch((err => {
+                console.error(err);
+                Alert.alert("error parsing qr");
+            }))
         }
     };
 
@@ -39,6 +50,7 @@ export default function GetPicture() {
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Button title="Pick an image from camera roll" onPress={pickImage} />
             {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            {data && <Text>{data}</Text>}
         </View>
     );
 }
